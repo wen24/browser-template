141f5d00c7eb105daaa7007f5990f35509a77ed3
wen24 <wenh93@gmail.com>
2017-08-12 17:16:41 -0400

    changes added api/prep for read/js broken

--- assets/game/api.js
+++ assets/game/api.js
@@ -1,4 +1,132 @@
 const app = require('../app.js')
 
+// Coptia'ss code,
+
+const addUser = function (data) {
+  // console.log(data)
+  return $.ajax({
+    url: app.host + '/sign-up/',
+    // headers: { 'header': 'Content-Type: application/json' },
+    method: 'POST',
+    data: {
+      }
+    }
+    // crossDomain: true
+  })
+}
+
+// api call for logging in suser
+const userLogin = function (data) {
+  /* let userInfo = {
+      Hello : "Hello World"
+  } */
+  // console.log(userInfo)
+  // console.log(data)
+  return $.ajax({
+    url: app.host + '/sign-in/',
+    method: 'POST',
+    data: {
+      'credentials': {
+        'email': data.credentials.email,
+        'password': data.credentials.password
+      }
+    } /* ,
+    success: function (response) {
+      console.log(response.user)
+      //userInfo = response.user
+      return userInfo
+    } */
+  })
+}
+
+// api call for resetting password
+const passwordReset = function (data) {
+  console.log(app.user.token)
+  // console.log(data)
+  return $.ajax({
+    url: app.host + '/change-password/' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token
+    },
+    method: 'PATCH',
+    data /* : {
+      "passwords": {
+      "old": data.passwords.old,
+      "new": data.passwords.new
+    }
+  } */
+  })
+}
+
+// api call for logging in
+const userLogout = function () {
+  // console.log('something')
+  return $.ajax({
+    url: app.host + '/sign-out/' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token
+    },
+    method: 'DELETE'
+  })
+}
+
+// api call for starting a new game
+const getGame = function () {
+  // console.log ("Hello")
+  console.log(app.user.token)
+  return $.ajax({
+    url: app.host + '/games',
+    headers: {
+      Authorization: 'Token token=' + app.user.token
+    },
+    method: 'POST',
+    success: function (response) {
+      console.log(response)
+    }
+  })
+}
+
+// event handler for adding moves to game object
+const updateMoves = function (index, value, over) {
+  console.log(app.user.token)
+  return $.ajax({
+    url: app.host + '/games/' + app.game.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + app.user.token
+    },
+    data: {
+      'game': {
+        'cell': {
+          'index': index,
+          'value': value
+        },
+        'over': over
+      }
+    }
+  })
+}
+
+// event handler for viewing games by user
+const gameView = function () {
+  console.log(app.user.token)
+  return $.ajax({
+    url: app.host + '/games',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + app.user.token
+    }
+  })
+}
 
 module.exports = {
+  addUser,
+  userLogin,
+  passwordReset,
+  userLogout,
+  getGame,
+  updateMoves,
+  gameView
+  // fetchUsers,
+  // getGameInProgress
+}
--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -64,16 +64,15 @@ $(document).ready(function () {
       $('.sq7').hasClass('x') || $('.sq7').hasClass('o') && $('.sq8').hasClass('x') || $('.sq8').hasClass('o') && $('.sq9').hasClass('x') || $('.sq9').hasClass('o') ===
       $(squareSelected.hasClass('x')) || $(squareSelected.hasClass('o'))
       return (document.getElementById('.displayWLT').innerHTML = 'Draw!'))
-    }
+  }
 
   function reset () {
     $('#reset').onclick
     $('.square').removeClass(('.sq1') && ('.sq2') && ('.sq3') && ('.sq4') && ('.sq5') && ('.sq6') && ('.sq7') && ('.sq8') && ('.sq9')) // remove (.sq1, .sq2, etc)
   }
 
-)} // close my global scope and allows access to everything yet error
-// added turns i need to i++
-// make sure each sq1 has x or o then
+)} // <--closing for global, moved around but still can't contain all functions in global scope.
+    // also not 100% is draw/reset working as intended
 
 module.exports = true
 checkIfPlayerWon,
